0167edd70d7ea80a510170fd5853ef6c29c42732
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-07-26 10:42:42 +0700

    feat: adminTemplate-header

--- src/templates/AdminTemplate.jsx
+++ src/templates/AdminTemplate.jsx
@@ -1,26 +1,55 @@
 import React, { useEffect, useState } from "react";
 import {
+  LogoutOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+  SettingOutlined,
+  UserOutlined,
   // UploadOutlined,
   // UserOutlined,
   // VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, Button, theme } from "antd";
+import { Layout, Menu, Button, theme, Dropdown } from "antd";
 import { NavLink, Outlet } from "react-router-dom";
 import { getLocal } from "../utils/localStore";
+import "./AdminTemplate.style.scss";
+
 const { Header, Sider, Content } = Layout;
 
 const AdminTemplate = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [admin, setAdmin] = useState();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  // ! Dropdown list
+  const items = [
+    {
+      icon: <UserOutlined />,
+      label: "My account",
+      key: "0",
+    },
+    {
+      icon: <SettingOutlined />,
+      label: "Setting",
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      icon: <LogoutOutlined />,
+      label: "Log out",
+      key: "3",
+    },
+  ];
 
   // ! Khi người dùng không phải là quản trị sẽ đá về trang chủ hoặc bất kì trang nào mình muốn
   useEffect(() => {
     // ! gọi dữ liệu từ local lên
     const user = getLocal("user");
+    setAdmin(user.hoTen);
     // ! một là không có dữ liệu
     // ! hai là lấy lên mà mã loại khách hàng không phải là quản trị
     if (user) {
@@ -31,6 +60,13 @@ const AdminTemplate = () => {
       window.location.href = "https://www.google.com.vn";
     }
   }, []);
+
+  const firstLetter = (string) => {
+    if (string) {
+      let strArr = string.split("");
+      return strArr[0].toUpperCase();
+    }
+  };
 
   return (
     <Layout className="min-h-screen">
@@ -66,16 +102,32 @@ const AdminTemplate = () => {
             background: colorBgContainer,
           }}
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
-          />
+          <div className="flex justify-between">
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+            <Dropdown
+              menu={{
+                items,
+              }}
+              trigger={["click"]}
+            >
+              <div
+                id="admin__user"
+                className="flex items-center pr-10 cursor-pointer"
+              >
+                <div id="admin__logo">{firstLetter(admin)}</div>
+                <p className="ml-3">{admin}</p>
+              </div>
+            </Dropdown>
+          </div>
         </Header>
         <Content
           style={{
